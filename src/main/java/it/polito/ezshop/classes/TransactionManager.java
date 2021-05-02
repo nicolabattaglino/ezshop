f7ca3bcbe7f429de6f33d71962ad9f9b18e06e52
STEFANO GRIVA s287729 <s287729@studenti.polito.it>
2021-05-02 10:51:21 +0200

    Set up transaction manage methods from return on

--- src/main/java/it/polito/ezshop/classes/TransactionManager.java
+++ src/main/java/it/polito/ezshop/classes/TransactionManager.java
@@ -1,5 +1,52 @@
 package it.polito.ezshop.classes;
 
+import it.polito.ezshop.exceptions.*;
+import it.polito.ezshop.data.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class TransactionManager {
-    
+    public Integer startReturnTransaction(Integer saleNumber) throws /*InvalidTicketNumberException,*/InvalidTransactionIdException, UnauthorizedException {
+        return null;
+    }
+
+    public boolean returnProduct(Integer returnId, String productCode, int amount) throws InvalidTransactionIdException, InvalidProductCodeException, InvalidQuantityException, UnauthorizedException {
+        return false;
+    }
+    public boolean endReturnTransaction(Integer returnId, boolean commit) throws InvalidTransactionIdException, UnauthorizedException {
+        return false;
+    }
+    public boolean deleteReturnTransaction(Integer returnId) throws InvalidTransactionIdException, UnauthorizedException {
+        return false;
+    }
+
+
+    public double receiveCashPayment(Integer ticketNumber, double cash) throws InvalidTransactionIdException, InvalidPaymentException, UnauthorizedException {
+        return 0;
+    }
+
+    public boolean receiveCreditCardPayment(Integer ticketNumber, String creditCard) throws InvalidTransactionIdException, InvalidCreditCardException, UnauthorizedException {
+        return false;
+    }
+
+    public double returnCashPayment(Integer returnId) throws InvalidTransactionIdException, UnauthorizedException {
+        return 0;
+    }
+
+    public double returnCreditCardPayment(Integer returnId, String creditCard) throws InvalidTransactionIdException, InvalidCreditCardException, UnauthorizedException {
+        return 0;
+    }
+
+    public boolean recordBalanceUpdate(double toBeAdded) throws UnauthorizedException {
+        return false;
+    }
+
+    public List<BalanceOperation> getCreditsAndDebits(LocalDate from, LocalDate to) throws UnauthorizedException {
+        return null;
+    }
+
+    public double computeBalance() throws UnauthorizedException {
+        return 0;
+    }
 }
