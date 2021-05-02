9e4463754abf36f981d06a9bb512f8adec17d367
STEFANO GRIVA s287729 <s287729@studenti.polito.it>
2021-05-02 12:57:25 +0200

    adde return implements balance operation

--- src/main/java/it/polito/ezshop/classes/ReturnTransaction.java
+++ src/main/java/it/polito/ezshop/classes/ReturnTransaction.java
@@ -5,6 +5,37 @@ import it.polito.ezshop.data.*;
 import java.time.LocalDate;
 import java.util.List;
 
-public class ReturnTransaction {
+public class ReturnTransaction implements BalanceOperation {
+    public int getBalanceId(){
+        return 0;
+    }
+
+    public void setBalanceId(int balanceId){
+        return;
+    }
+
+    public LocalDate getDate(){
+    return null;
+    }
+
+    public void setDate(LocalDate date){
+        return;
+    }
+
+    public double getMoney(){
+        return 0;
+    }
+
+    public void setMoney(double money){
+        return;
+    }
+
+    public String getType(){
+        return"";
+    }
+
+    public void setType(String type){
+        return;
+    }
     
 }
