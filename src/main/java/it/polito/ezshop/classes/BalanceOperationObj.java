59bc66a3f42971db3b663f7030181ebdc048b1bb
Mattia <mattiacaracciolo1@gmail.com>
2021-05-08 01:30:43 +0200

    added package classes

--- src/main/java/it/polito/ezshop/classes/BalanceOperationObj.java
+++ src/main/java/it/polito/ezshop/classes/BalanceOperationObj.java
@@ -1,53 +1,45 @@
 package it.polito.ezshop.classes;
 
 import it.polito.ezshop.data.BalanceOperation;
-import it.polito.ezshop.exceptions.*;
-import it.polito.ezshop.data.*;
 
 import java.time.LocalDate;
-import java.util.*;
-import it.polito.ezshop.data.*;
 
 public abstract class BalanceOperationObj implements BalanceOperation {
     private int balanceId;
     private LocalDate date;
     private double money;
     private String type;
-
-    public int getBalanceId(){
+    
+    public int getBalanceId() {
         return balanceId;
     }
-
-    public void setBalanceId(int balanceId){
+    
+    public void setBalanceId(int balanceId) {
         this.balanceId = balanceId;
-        return;
     }
-
-    public LocalDate getDate(){
-    return date;
+    
+    public LocalDate getDate() {
+        return date;
     }
-
-    public void setDate(LocalDate date){
-        this.date=date;
-        return;
+    
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
-
-    public double getMoney(){
+    
+    public double getMoney() {
         return money;
     }
-
-    public void setMoney(double money){
-        this.money=money;
-        return;
+    
+    public void setMoney(double money) {
+        this.money = money;
     }
-
-    public String getType(){
+    
+    public String getType() {
         return type;
     }
-
-    public void setType(String type){
-        this.type=type;
-        return;
+    
+    public void setType(String type) {
+        this.type = type;
     }
     
     
--- src/main/java/it/polito/ezshop/classes/OrderObj.java
+++ src/main/java/it/polito/ezshop/classes/OrderObj.java
@@ -1,10 +1,6 @@
 package it.polito.ezshop.classes;
-import it.polito.ezshop.exceptions.*;
-import it.polito.ezshop.data.*;
 
-import java.time.LocalDate;
-import java.util.*;
-import it.polito.ezshop.data.*;
+import it.polito.ezshop.data.Order;
 
 public class OrderObj implements Order {
     private Integer balanceId;
@@ -14,57 +10,52 @@ public class OrderObj implements Order {
     private String status;
     private Integer orderId;
     
-     public String getProductCode(){
+    public String getProductCode() {
         return productCode;
     }
-
-    public void setProductCode(String productCode){
-        this.productCode=productCode;
-        return;
+    
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
     }
-
-    public double getPricePerUnit(){
+    
+    public double getPricePerUnit() {
         return pricePerUnit;
     }
-
-    public void setPricePerUnit(double pricePerUnit){
-        this.pricePerUnit=pricePerUnit;
-        return;
+    
+    public void setPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
     }
-
-    public int getQuantity(){
+    
+    public int getQuantity() {
         return quantity;
     }
-
-    public void setQuantity(int quantity){
-        this.quantity=quantity;
-        return;
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
-
-    public String getStatus(){
+    
+    public String getStatus() {
         return status;
     }
-
-    public void setStatus(String status){
-        this.status=status;
-        return;
+    
+    public void setStatus(String status) {
+        this.status = status;
     }
-
-    public Integer getOrderId(){
+    
+    public Integer getOrderId() {
         return orderId;
     }
-
-    public void setOrderId(Integer orderId){
-        this.orderId=orderId;
-        return;
+    
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
     }
+    
     @Override
-    public Integer getBalanceId(){
+    public Integer getBalanceId() {
         return balanceId;
     }
-
-    public void setBalanceId(Integer balanceId){
-        this.balanceId=balanceId;
-        return;
+    
+    public void setBalanceId(Integer balanceId) {
+        this.balanceId = balanceId;
     }
 }
