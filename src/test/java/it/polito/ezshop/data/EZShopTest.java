3499c2288504a5af7a7a12cbee4bb6a2ab15667b
Mattia <mattiacaracciolo1@gmail.com>
2021-05-21 10:47:04 +0200

    added EZShopTest.java

--- src/test/java/it/polito/ezshop/data/EZShopTest.java
+++ src/test/java/it/polito/ezshop/data/EZShopTest.java
@@ -1,7 +1,224 @@
 package it.polito.ezshop.data;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EZShopTest {
-
+    
+    @Test
+    public void getTransactionManager() {
+    }
+    
+    @Test
+    public void getCustomerManager() {
+    }
+    
+    @Test
+    public void getUserManager() {
+    }
+    
+    @Test
+    public void reset() {
+    }
+    
+    @Test
+    public void createUser() {
+    }
+    
+    @Test
+    public void deleteUser() {
+    }
+    
+    @Test
+    public void getAllUsers() {
+    }
+    
+    @Test
+    public void getUser() {
+    }
+    
+    @Test
+    public void updateUserRights() {
+    }
+    
+    @Test
+    public void login() {
+    }
+    
+    @Test
+    public void getProductOrderManager() {
+    }
+    
+    @Test
+    public void logout() {
+    }
+    
+    @Test
+    public void createProductType() {
+    }
+    
+    @Test
+    public void updateProduct() {
+    }
+    
+    @Test
+    public void deleteProductType() {
+    }
+    
+    @Test
+    public void getAllProductTypes() {
+    }
+    
+    @Test
+    public void getProductTypeByBarCode() {
+    }
+    
+    @Test
+    public void getProductTypesByDescription() {
+    }
+    
+    @Test
+    public void updateQuantity() {
+    }
+    
+    @Test
+    public void updatePosition() {
+    }
+    
+    @Test
+    public void issueOrder() {
+    }
+    
+    @Test
+    public void payOrderFor() {
+    }
+    
+    @Test
+    public void payOrder() {
+    }
+    
+    @Test
+    public void recordOrderArrival() {
+    }
+    
+    @Test
+    public void getAllOrders() {
+    }
+    
+    @Test
+    public void defineCustomer() {
+    }
+    
+    @Test
+    public void modifyCustomer() {
+    }
+    
+    @Test
+    public void deleteCustomer() {
+    }
+    
+    @Test
+    public void getCustomer() {
+    }
+    
+    @Test
+    public void getAllCustomers() {
+    }
+    
+    @Test
+    public void createCard() {
+    }
+    
+    @Test
+    public void attachCardToCustomer() {
+    }
+    
+    @Test
+    public void modifyPointsOnCard() {
+    }
+    
+    @Test
+    public void startSaleTransaction() {
+    }
+    
+    @Test
+    public void addProductToSale() {
+    }
+    
+    @Test
+    public void deleteProductFromSale() {
+    }
+    
+    @Test
+    public void applyDiscountRateToProduct() {
+    }
+    
+    @Test
+    public void applyDiscountRateToSale() {
+    }
+    
+    @Test
+    public void computePointsForSale() {
+    }
+    
+    @Test
+    public void endSaleTransaction() {
+    }
+    
+    @Test
+    public void deleteSaleTransaction() {
+    }
+    
+    @Test
+    public void getSaleTransaction() {
+    }
+    
+    @Test
+    public void startReturnTransaction() {
+    }
+    
+    @Test
+    public void returnProduct() {
+    }
+    
+    @Test
+    public void endReturnTransaction() {
+    }
+    
+    @Test
+    public void deleteReturnTransaction() {
+    }
+    
+    @Test
+    public void receiveCashPayment() {
+    }
+    
+    @Test
+    public void receiveCreditCardPayment() {
+    }
+    
+    @Test
+    public void returnCashPayment() {
+    }
+    
+    @Test
+    public void returnCreditCardPayment() {
+    }
+    
+    @Test
+    public void recordBalanceUpdate() {
+    }
+    
+    @Test
+    public void getCreditsAndDebits() {
+    }
+    
+    @Test
+    public void computeBalance() {
+    }
+    
+    @Test
+    public void addOrder() {
+    }
 }
