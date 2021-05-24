b2f5b0ed9cd41a9fac6d3ec7f0b8b6c079c473c1
Nicola <s287468@studenti.polito.it>
2021-05-24 21:48:22 +0200

    test classes added

--- src/test/java/it/polito/ezshop/classes/CustomerManagerTest.java
+++ src/test/java/it/polito/ezshop/classes/CustomerManagerTest.java
@@ -1,5 +1,9 @@
 package it.polito.ezshop.classes;
 
+import it.polito.ezshop.exceptions.InvalidCustomerCardException;
+import it.polito.ezshop.exceptions.InvalidCustomerIdException;
+import it.polito.ezshop.exceptions.InvalidCustomerNameException;
+import it.polito.ezshop.exceptions.UnauthorizedException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,35 +11,118 @@ import static org.junit.Assert.*;
 public class CustomerManagerTest {
 
     @Test
-    public void defineCustomer() {
+    public void defineCustomer() throws InvalidCustomerNameException, InvalidCustomerIdException, UnauthorizedException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        assertThrows(InvalidCustomerNameException.class, () -> cm.defineCustomer(""));
+        assertThrows(InvalidCustomerNameException.class, () -> cm.defineCustomer(null));
+        int customerId = cm.defineCustomer("PaulJ");
+        assertEquals(0, (int) cm.getCustomer(customerId).getId());
+        assertEquals(-1, (int) cm.defineCustomer("PaulJ"));
+        assertEquals(1, (int) cm.defineCustomer("MikeP"));
+
     }
 
     @Test
-    public void modifyCustomer() {
+    public void modifyCustomer() throws InvalidCustomerIdException, InvalidCustomerNameException, UnauthorizedException, InvalidCustomerCardException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        cm.createCard();
+        cm.createCard();
+        cm.createCard();
+        int customerId = cm.defineCustomer("MIkeR");;
+        assertThrows(InvalidCustomerIdException.class, () -> cm.modifyCustomer(-1, "MikeR", "1000000009"));
+        assertThrows(InvalidCustomerNameException.class, () -> cm.modifyCustomer(1, "", "1000000009"));
+        assertThrows(InvalidCustomerNameException.class, () -> cm.modifyCustomer(1, null, "1000000009"));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.modifyCustomer(1, "MikeR", "111"));
+        assertFalse(cm.modifyCustomer(customerId,"MikeR", "1000000009"));
+        int customerId1 =  cm.defineCustomer("DanyT");;
+        cm.attachCardToCustomer("1000000001",customerId1);
+        assertFalse(cm.modifyCustomer(customerId, "MikeR","1000000001"));
+        assertTrue(cm.modifyCustomer(customerId, "BobN", null));
+        assertFalse(cm.attachCardToCustomer("1000000003",0));
+        cm.attachCardToCustomer("1000000002",0);
+        assertTrue(cm.modifyCustomer(customerId, "BobN", ""));
+        assertTrue(cm.modifyCustomer(customerId, "BobN", "1000000000"));
     }
 
     @Test
-    public void deleteCustomer() {
+    public void deleteCustomer() throws InvalidCustomerNameException, InvalidCustomerIdException, UnauthorizedException, InvalidCustomerCardException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        assertFalse(cm.deleteCustomer(0));
+        int id = cm.defineCustomer("MIkeR");
+        createCard();
+        cm.attachCardToCustomer("1000000000",id);
+        int id1 = cm.defineCustomer("Paul");
+        assertThrows(InvalidCustomerIdException.class, () -> cm.deleteCustomer(-1));
+        assertTrue(cm.deleteCustomer(id));
+        assertTrue(cm.deleteCustomer(id1));
     }
 
     @Test
-    public void getCustomer() {
+    public void getCustomer() throws InvalidCustomerIdException, UnauthorizedException, InvalidCustomerNameException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        assertThrows(InvalidCustomerIdException.class, () -> cm.getCustomer(-1));
+        assertNull(cm.getCustomer(0));
+        int id = cm.defineCustomer("MIkeR");
+        assertEquals(id, (int) cm.getCustomer(id).getId());
+
     }
 
     @Test
-    public void getAllCustomers() {
+    public void getAllCustomers() throws InvalidCustomerNameException, UnauthorizedException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        int id = cm.defineCustomer("MIkeR");
+        assertEquals(id, (int) cm.getAllCustomers().get(id).getId());
     }
 
     @Test
     public void createCard() {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        assertEquals("1000000000", cm.createCard());
+        assertEquals("1000000001", cm.createCard());
+
     }
 
     @Test
-    public void attachCardToCustomer() {
+    public void attachCardToCustomer() throws InvalidCustomerIdException, UnauthorizedException, InvalidCustomerCardException, InvalidCustomerNameException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+
+        assertThrows(InvalidCustomerIdException.class, () -> cm.attachCardToCustomer("1000000000",-1));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.attachCardToCustomer("",0));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.attachCardToCustomer("t",0));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.attachCardToCustomer(null,0));
+        assertFalse(cm.attachCardToCustomer("1000000000",1));
+
+        // test is attached
+        cm.createCard();
+        cm.createCard();
+        cm.createCard();
+        int customerId = cm.defineCustomer("MIkeR");
+        int customerId1 =  cm.defineCustomer("DanyT");
+        assertTrue(cm.attachCardToCustomer("1000000000",customerId));
+        assertFalse(cm.attachCardToCustomer("1000000000",customerId1));
+
     }
 
     @Test
-    public void modifyPointsOnCard() {
+    public void modifyPointsOnCard() throws UnauthorizedException, InvalidCustomerCardException, InvalidCustomerNameException, InvalidCustomerIdException {
+        CustomerManager cm = new CustomerManager(null);
+        cm.clear();
+        assertThrows(InvalidCustomerCardException.class, () -> cm.modifyPointsOnCard("",10));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.modifyPointsOnCard(null,10));
+        assertThrows(InvalidCustomerCardException.class, () -> cm.modifyPointsOnCard("t",10));
+        assertFalse(cm.modifyPointsOnCard("1000000000", 10));
+        int customerId = cm.defineCustomer("MIkeR");
+        cm.createCard();
+        cm.attachCardToCustomer("1000000000",customerId);
+        assertTrue(cm.modifyPointsOnCard("1000000000",10));
+
     }
 
     @Test
--- src/test/java/it/polito/ezshop/classes/UserManagerTest.java
+++ src/test/java/it/polito/ezshop/classes/UserManagerTest.java
@@ -1,14 +1,89 @@
 package it.polito.ezshop.classes;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import it.polito.ezshop.data.User;
-import it.polito.ezshop.exceptions.InvalidPasswordException;
-import it.polito.ezshop.exceptions.InvalidRoleException;
-import it.polito.ezshop.exceptions.InvalidUsernameException;
+import it.polito.ezshop.exceptions.*;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+
+import java.util.LinkedList;
 
 import static org.junit.Assert.*;
 
 public class UserManagerTest {
+
+
+    @Test
+    public void testGetUserLogged() throws InvalidPasswordException, InvalidUsernameException, InvalidRoleException {
+        UserManager um = new UserManager(null);
+        um.createUser("SimAdmin","12345","Administrator");
+        User userLogged = um.login("SimAdmin", "12345");
+        assertEquals(um.getUserLogged().getUsername(),userLogged.getUsername());
+    }
+
+    @Test
+    public void testCreateUser() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException, UnauthorizedException {
+        UserManager um = new UserManager(null);
+        um.clear();
+        um.createUser("SimAdmin","12345","Administrator");
+
+        assertThrows(InvalidUsernameException.class, () -> um.createUser("","1234","Cashier"));
+        assertThrows(InvalidUsernameException.class, () -> um.createUser(null,"1234","Cashier"));
+        assertThrows(InvalidPasswordException.class, () -> um.createUser("John","","Cashier"));
+        assertThrows(InvalidPasswordException.class, () -> um.createUser("John",null,"Cashier"));
+        assertThrows(InvalidRoleException.class, () -> um.createUser("John","1234",""));
+        assertThrows(InvalidRoleException.class, () -> um.createUser("John","1234",null));
+        assertThrows(InvalidRoleException.class, () -> um.createUser("John","1234","employee"));
+        assertEquals(-1, (int) um.createUser("SimAdmin", "12345", "Administrator"));
+        assertEquals(um.getAllUsers().size(), (int) um.createUser("John", "54321", "Cashier"));
+    }
+
+    @Test
+    public void testDeleteUser() throws InvalidUserIdException, UnauthorizedException, InvalidPasswordException, InvalidRoleException, InvalidUsernameException {
+        UserManager um = new UserManager(null);
+        um.createUser("SimAdmin","12345","Administrator");
+        um.createUser("John", "54321", "Cashier");
+        assertThrows(InvalidUserIdException.class, () -> um.deleteUser(-2));
+        assertThrows(InvalidUserIdException.class, () -> um.deleteUser(null));
+        assertTrue(um.deleteUser(1));
+    }
+
+    @Test
+    public void testGetAllUsers() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException, UnauthorizedException {
+        UserManager um = new UserManager(null);
+        um.clear();
+        um.createUser("SimAdmin","12345","Administrator");
+        um.createUser("John", "54321", "Cashier");
+        LinkedList <User> userList;
+        userList = (LinkedList<User>) um.getAllUsers();
+        assertEquals(2, userList.size());
+    }
+
+    @Test
+    public void testGetUser() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException, InvalidUserIdException, UnauthorizedException {
+        UserManager um = new UserManager(null);
+        um.clear();
+        um.createUser("SimAdmin","12345","Administrator");
+        int userId = um.createUser("John", "54321", "Cashier");
+        assertThrows(InvalidUserIdException.class, () -> um.getUser(-1));
+        assertEquals("John", um.getUser(userId).getUsername());
+        assertNull(um.getUser(10));
+    }
+
+    @Test
+    public void testUpdateUserRights() throws InvalidPasswordException, InvalidRoleException, InvalidUsernameException, InvalidUserIdException, UnauthorizedException {
+        UserManager um = new UserManager(null);
+        um.clear();
+        um.createUser("SimAdmin","12345","Administrator");
+        int userId = um.createUser("John", "54321", "Cashier");
+        assertThrows(InvalidUserIdException.class, () -> um.updateUserRights(-1, "Administrator"));
+        assertThrows(InvalidUserIdException.class, () -> um.updateUserRights(null,"Administrator"));
+        assertThrows(InvalidRoleException.class, () -> um.updateUserRights(0,""));
+        assertThrows(InvalidRoleException.class, () -> um.updateUserRights(0,null));
+        assertThrows(InvalidRoleException.class, () -> um.updateUserRights(0,null));
+        assertThrows(InvalidRoleException.class, () -> um.updateUserRights(0,"t"));
+        assertTrue(um.updateUserRights(userId,"ShopManager"));
+    }
 
     @Test
     public void testLogin() throws InvalidPasswordException, InvalidUsernameException, InvalidRoleException {
@@ -31,39 +106,9 @@ public class UserManagerTest {
         assertTrue(um.logout());
     }
 
-    @Test
-    public void getUserLogged() {
-    }
-
-    @Test
-    public void createUser() {
-    }
-
-    @Test
-    public void deleteUser() {
-    }
-
-    @Test
-    public void getAllUsers() {
-    }
-
-    @Test
-    public void getUser() {
-    }
-
-    @Test
-    public void updateUserRights() {
-    }
-
-    @Test
-    public void login() {
-    }
-
-    @Test
-    public void logout() {
-    }
 
     @Test
     public void clear() {
+
     }
 }
